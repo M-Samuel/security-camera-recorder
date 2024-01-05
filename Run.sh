b8cb45ceb174197fef43924154e98cfcf6f66ebd
samuel.modeste <samuel.m.modeste@outlook.com>
2024-01-05 21:08:10 +0400

    Added memory mount to reduce disk wear

--- Run.sh
+++ Run.sh
@@ -5,7 +5,7 @@ ffmpeg -rtsp_transport tcp -i $RTSP  -c:v copy -c:a aac -reset_timestamps 1 -f s
 P1=$!
 
 # upload file to amazon s3 and delete old files
-while true; do aws s3 sync /recordings s3://$S3_BUCKET/$S3_DIRECTORY && find /recordings -name '*.mp4' -type f -mmin +5 -delete -print; sleep $SYNC_S3_AFTER_SECONDS; done &
+while true; do aws s3 sync /recordings s3://$S3_BUCKET/$S3_DIRECTORY && find /recordings -name '*.mp4' -type f -mmin +$DELETE_FILE_OLDER_MIN -delete -print; sleep $SYNC_S3_AFTER_SECONDS; done &
 P2=$!
 
 wait $P1 $P2
