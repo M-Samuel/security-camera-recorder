947ca4e51a02f506f0301d6a83bfd8c30e32a5e5
samuel.modeste <samuel.m.modeste@outlook.com>
2024-01-27 14:22:01 +0400

    Fix rename script

--- rename.sh
+++ rename.sh
@@ -13,18 +13,19 @@ fi
 echo "Finding files in the source directory that have not been modified in the last minute..."
 # Find files in the source directory that have not been modified in the last minute
 # and move them to the target directory
-find "$SOURCE_DIR" -type f -mmin +1 -exec mv {} "$TARGET_DIR" \;
-echo "Files moved successfully!"
+count=0
+find "$SOURCE_DIR" -type f -mmin +1 -exec mv {} "$TARGET_DIR" \; -exec echo "Moved file: {}" \; -exec sh -c 'count=$((count+1))' \;
+echo "Files moved successfully! Total files moved: $count"
 
-echo "Checking if files have unix timestamp and converting it into yyyyMMddHHmmss format in the Mauritian time zone..."
-# Check if files have unix timestamp and convert it into yyyyMMddHHmmss format in the Mauritian time zone
+
+# Check all files in target directory
 for file in "$TARGET_DIR"/*; do
-    if [[ -f "$file" ]]; then
-        timestamp=$(stat -c %Y "$file")
-        formatted_date=$(TZ=Indian/Mauritius date -d "@$timestamp" +"%Y%m%d%H%M%S")
-        new_filename="${file%.*}_$formatted_date.${file##*.}"
-        mv "$file" "$new_filename"
-        echo "Renamed file: $file to $new_filename"
+    if [[ $file =~ stream_piece_([0-9]+)\.mp4 ]]; then
+        timestamp=${BASH_REMATCH[1]}
+        # Convert the unix timestamp to yyyyMMddHHmmss using the Mauritian time zone
+        formatted_timestamp=$(TZ=Indian/Mauritius date -d @$timestamp +'%Y%m%d%H%M%S')
+        # Rename the file
+        echo "Renaming file: $file to stream_piece_$formatted_timestamp.mp4"
+        mv "$file" "$TARGET_DIR/stream_piece_$formatted_timestamp.mp4"
     fi
 done
-echo "Conversion completed!"
