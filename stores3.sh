5e8b8a92c30f16befe8e1f00e972ed0b4442c559
samuel.modeste <samuel.m.modeste@outlook.com>
2024-01-27 10:22:36 +0400

    Fix py path

--- stores3.sh
+++ stores3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-aws s3 sync /recordings s3://$S3_BUCKET/$S3_DIRECTORY && find /recordings -name '*.mp4' -type f -mmin +$DELETE_FILE_OLDER_MIN -delete -print && python rename.py
+aws s3 sync /recordings s3://$S3_BUCKET/$S3_DIRECTORY && find /recordings -name '*.mp4' -type f -mmin +$DELETE_FILE_OLDER_MIN -delete -print && python /scripts/rename.py
 sleep $SYNC_S3_AFTER_SECONDS
 echo "S3 Bucket Upload Respawning.."
 done
