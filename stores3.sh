215ca08b64f2afd19c4837ae957adefbffcaa7f1
samuel.modeste <samuel.m.modeste@outlook.com>
2024-01-28 09:17:48 +0400

    Reduce cost using standard infrequent access

--- stores3.sh
+++ stores3.sh
@@ -2,7 +2,7 @@
 while true
 do
 /scripts/rename.sh
-aws s3 sync $UPLOAD_DIRECTORY s3://$S3_BUCKET/$S3_DIRECTORY && rm -f $UPLOAD_DIRECTORY/*
+aws s3 sync --storage-class $UPLOAD_DIRECTORY STANDARD_IA s3://$S3_BUCKET/$S3_DIRECTORY && rm -f $UPLOAD_DIRECTORY/*
 sleep $SYNC_S3_AFTER_SECONDS
 echo "S3 Bucket Upload Respawning.."
 done
