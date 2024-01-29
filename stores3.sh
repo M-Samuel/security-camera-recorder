3f567282655120f130b579f3f675a63b112b8121
samuel.modeste <samuel.m.modeste@outlook.com>
2024-01-29 06:18:03 +0400

    Added retries to wait for ffmpeg restart

--- stores3.sh
+++ stores3.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 while true
 do
-    if ! pgrep -x "ffmpeg" > /dev/null; then
-        echo "ffmpeg is not running, exiting.."
-        break
-    fi
+    retries=0
+    while true
+    do
+        if ! pgrep -x "ffmpeg" > /dev/null; then
+            echo "ffmpeg is not running, waiting for 10s.."
+            sleep 10
+            retries=$((retries+1))
+            if [ $retries -eq 3 ]; then
+                echo "Reached maximum retries, exiting.."
+                echo "ffmpeg is not running, exiting.."
+                break 2
+            fi
+        else
+            break
+        fi
+    done
+
+    /scripts/rename.sh
+    aws s3 sync --storage-class STANDARD_IA $UPLOAD_DIRECTORY s3://$S3_BUCKET/$S3_DIRECTORY && rm -f $UPLOAD_DIRECTORY/*
+    sleep $SYNC_S3_AFTER_SECONDS
+    echo "S3 Bucket Upload Respawning.."
+done
+    
+
+    # while true
+    # do
+    #     if ! pgrep -x "ffmpeg" > /dev/null; then
+    #         echo "ffmpeg is not running, waiting for 5s.."
+    #         sleep 5
+    #         retries=$((retries+1))
+    #         if [ $retries -eq 3 ]; then
+    #             echo "Reached maximum retries, exiting.."
+    #             echo "ffmpeg is not running, exiting.."
+    #             break
+    #             break
+    #         fi
+    #     fi
+
+    # done
     /scripts/rename.sh
     aws s3 sync --storage-class STANDARD_IA $UPLOAD_DIRECTORY s3://$S3_BUCKET/$S3_DIRECTORY && rm -f $UPLOAD_DIRECTORY/*
     sleep $SYNC_S3_AFTER_SECONDS
